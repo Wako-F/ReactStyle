4e4bf381b40397a667a195210f8b88a5d9bb5503
Wako-F <100088116+Wako-F@users.noreply.github.com>
2022-07-17 12:42:31 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0PWUpMlEaC9SYA1VuUwOzbF0bCi2wpRnpLg&usqp=CAU"
+      className="image"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0PWUpMlEaC9SYA1VuUwOzbF0bCi2wpRnpLg&usqp=CAU"
+      className="image"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0PWUpMlEaC9SYA1VuUwOzbF0bCi2wpRnpLg&usqp=CAU"
+      className="image"
+    />
+  </div>,
+  document.getElementById("root")
+);
